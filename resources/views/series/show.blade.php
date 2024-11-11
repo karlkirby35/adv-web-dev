d22347237da3b1fad5de85fd7f49685c5f81e5a4
karlkirby35 <157364782+karlkirby35@users.noreply.github.com>
2024-11-11 17:41:56 +0000

    commit

--- resources/views/series/show.blade.php
+++ resources/views/series/show.blade.php
@@ -9,6 +9,7 @@
     <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900">
+          
                 <h3 class="font-semibold text-lg mb-4">Series Details:</h3>
                     <x-series-details
                         :title="$series->title"
@@ -16,7 +17,11 @@
                         :year="$series->year"
                         :description="$series->description"
                     />
-                   
+                     <div class="mt-4">
+                        <a href="{{ route('series.index') }}" class="bg-black-300 hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 rounded border">
+                            Back to Homepage 
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
